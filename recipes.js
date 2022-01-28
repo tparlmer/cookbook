ad1426d8e61849e4767e1e568615fe5b176cb668
tparlmer <tparlmer@gmail.com>
2022-01-28 17:23:11 -0500

    second commit

--- recipes.js
+++ recipes.js
@@ -11,12 +11,28 @@ const recipes = {
     }
   ]
 }
-console.log("jello world")
+
+/*
+
 const recipesBox = document.getElementById("recipes");
-for (let i = 0; i < recipes.length; i ++) {
+for (let i = 0; i < recipes.ingredients.length; i ++) {
   const div = document.createElement("div");
   div.innerHTML = recipes[i].ingredient;
 
   recipesBox.appendChild(div);
 }
+*/
 
+document.getElementById("button").addEventListener("click", function(){
+
+  const para = document.createElement("p");
+  para.innerText = "This is a recipe";
+  document.body.appendChild(para);
+
+});
+
+document.getElementById("boxButton").addEventListener("click", function(){
+
+  document.getElementById("box").style.backgroundColor = "blue";
+
+});
